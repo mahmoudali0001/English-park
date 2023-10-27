6f6ff8d6d40ae39eacde45a718bc6ccea91e69e5
Mahmoud Ali <mahmoudalifarag1@gmail.com>
2023-10-27 18:51:18 +0200

    push the project to github

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-6xl font-bold">
+        ASkot ya <span className="text-purple-400 capitalize">zikkaaaaaaa</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
