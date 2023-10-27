c92a10a339786c9992b5ee91942ba7063c6f1ac6
Zex <59110684+ZEXAWY@users.noreply.github.com>
2023-10-27 19:03:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-6xl font-bold">
-        ASkot ya <span className="text-purple-400 capitalize">zikkaaaaaaa</span>
+        ASkot ya <span className="text-purple-400 capitalize">7oda</span>
       </h1>
     </div>
   );
