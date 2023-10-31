da43706b3b7c1cafe9c2d29149bf34fbec3dc225
Mahmoud Ali <mahmoudalifarag1@gmail.com>
2023-10-31 18:53:25 +0200

    Test

--- src/Containers/HomePage/HomePage.js
+++ src/Containers/HomePage/HomePage.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import ProductsCards from '../../Compononts/ProductsCards/ProductsCards';
-import HeaderCardComponent from '../../Compononts/HeaderCard/HeaderCard';
+import ProductsCards from "../../Compononts/ProductsCards/ProductsCards";
+import HeaderCardComponent from "../../Compononts/HeaderCard/HeaderCard";
 
 class HomePage extends Component {
   render() {
-    return <div className='text-center text-orange-300 text-6xl'>Home js</div>;
+    return (
+      <div className="text-center text-orange-300 text-6xl">
+        Home js 3ash zeka
+      </div>
+    );
   }
 }
 
