570de47e6f7272780c46c5a623d24e70114ee983
mahmoudali0001 <59316869+mahmoudali0001@users.noreply.github.com>
2023-10-31 17:53:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-6xl font-bold">
-        ASkot ya <span className="text-purple-400 capitalize">7oda</span>
-      </h1>
     </div>
   );
 }
