5462fe01242e008e8a6cf2d2621ad026c5e4745a
mahmoudali0001 <59316869+mahmoudali0001@users.noreply.github.com>
2023-10-31 17:59:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import Layout from "./hoc/Layout/Layout";
+import HomePage from "./Containers/HomePage/HomePage";
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <Routes>
+          <Route path="/" exact Component={HomePage} />
+          <Route
+            path="/shap"
+            element={
+              <p className="text-center text-orange-300 text-6xl">Shap</p>
+            }
+          />
+          <Route
+            path="/aboutus"
+            element={
+              <p className="text-center text-orange-300 text-6xl">About Us</p>
+            }
+          />
+          <Route
+            path="/contactus"
+            element={
+              <p className="text-center text-orange-300 text-6xl">Contact Us</p>
+            }
+          />
+        </Routes>
+      </Layout>
+    );
+  }
 }
 
 export default App;
