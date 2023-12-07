938ab9db0cd26617a7139fcb76ed476f81f1b519
Mahmoud Ali <mahmoudalifarag1@gmail.com>
2023-12-07 20:22:51 +0200

    fix some bug

--- src/Compononts/HomePageComponents/QUAALFIED TEACHRS/QualfiedTeachrsCards.js
+++ src/Compononts/HomePageComponents/QUAALFIED TEACHRS/QualfiedTeachrsCards.js
@@ -3,11 +3,20 @@ import React from "react";
 import QualfiedTeachrsCard from "./QUAALFIED TEACHRS CARD/QualfiedTeachrsCard";
 
 const QualfiedTeachrsCards = (props) => (
-  <section className="flex justify-evenly text-white py-16 relative">
-    <QualfiedTeachrsCard />
-    <QualfiedTeachrsCard />
-    <QualfiedTeachrsCard />
-    <QualfiedTeachrsCard />
+  <section className="py-10">
+    <article className="text-center">
+      <span className="text-base text-cyan-500 font-bold">Our Team</span>
+
+      <p className="text-center font-bold text-2xl">
+        Meet our team and teacher who <br /> teaches you online
+      </p>
+    </article>
+    <article className="flex flex-row py-16 relative justify-evenly ">
+      <QualfiedTeachrsCard />
+      <QualfiedTeachrsCard />
+      <QualfiedTeachrsCard />
+      <QualfiedTeachrsCard />
+    </article>
   </section>
 );
 
